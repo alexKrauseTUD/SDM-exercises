f750c86112608f04a47f2e9d02495b4984e73e5a
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2025-10-26 16:36:36 +0100

    Changed setup.sh to reflect changes to devcontainer
    Pull instead of build

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t sdmcontainer --build-arg TIMEZONE=$(cat /etc/timezone) .
+#docker build -t sdmcontainer --build-arg TIMEZONE=$(cat /etc/timezone) ./.devcontainer
+
+docker pull jpietrzyktud/sdm:latest
+docker tag jpietrzyktud/sdm:latest sdmcontainer:latest
